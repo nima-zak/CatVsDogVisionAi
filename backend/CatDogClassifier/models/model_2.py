666149f73349c62f6cc8a4ca5c8e747596646bec
nima-zak <z.nima27@gmail.com>
2024-01-13 23:02:50 -0600

    Improved model training script with new 3 models configurations

---
+++ backend/CatDogClassifier/models/model_2.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+
+# Define the model
+def build_model_2():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    # Change the output layer for categorical classification
+    model.add(Dense(2, activation='softmax'))
+
+     # Change the loss function to categorical_crossentropy
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
+
+    return model
+
+model_2 = build_model_2()
+model_2.summary()
